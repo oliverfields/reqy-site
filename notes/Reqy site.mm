4054b3ed0abaaae9ef6fc7fa5d8c5513a4d52cf1
Oliver Fields <oliver@phnd.net>
2012-09-16 21:54:58 +0200

    Initial

---
+++ notes/Reqy site.mm
@@ -0,0 +1,110 @@
+<map version="0.9.0">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1344943406815" ID="ID_1101580145" MODIFIED="1344943611579" TEXT="Reqy site">
+<hook NAME="MapStyle"/>
+<node CREATED="1344943422854" ID="ID_1898334152" MODIFIED="1344943830403" POSITION="right" TEXT="Tutorial">
+<node CREATED="1344946407548" ID="ID_1574523983" MODIFIED="1344946437291" TEXT="Walkthrough fictious software lifecycle"/>
+</node>
+<node CREATED="1344943586385" ID="ID_1049785723" MODIFIED="1344946515411" POSITION="left" TEXT="User manual">
+<node CREATED="1344944321851" ID="ID_1243906578" MODIFIED="1345115530166" TEXT="Repository overview">
+<attribute NAME="Priority" VALUE="Must"/>
+<attribute NAME="Rationale" VALUE="Good to have really"/>
+<node CREATED="1344944338194" ID="ID_836565848" MODIFIED="1344944340896" TEXT="Requirements">
+<node CREATED="1344946532941" ID="ID_1788434308" MODIFIED="1344946537971" TEXT="Attributes">
+<node CREATED="1344946539466" ID="ID_1271976215" MODIFIED="1344946543401" TEXT="Linklist"/>
+<node CREATED="1344946543642" ID="ID_965086698" MODIFIED="1344946545256" TEXT="Date"/>
+<node CREATED="1344946545662" ID="ID_326902650" MODIFIED="1344946547127" TEXT="String"/>
+</node>
+</node>
+<node CREATED="1344944341470" ID="ID_923402463" MODIFIED="1344944345467" TEXT="Requirement packages"/>
+<node CREATED="1344944345911" ID="ID_180319768" MODIFIED="1344944347835" TEXT="Documents"/>
+<node CREATED="1344944348228" ID="ID_1400407312" MODIFIED="1344944351186" TEXT="Stakeholders"/>
+<node CREATED="1344944351964" ID="ID_138042487" MODIFIED="1344944354932" TEXT="Glossary"/>
+</node>
+<node CREATED="1344944377969" ID="ID_1506023094" MODIFIED="1344944383416" TEXT="Modes">
+<node CREATED="1344944384539" ID="ID_25418482" MODIFIED="1344944393820" TEXT="Artifact"/>
+<node CREATED="1344944394248" ID="ID_41091007" MODIFIED="1344944398483" TEXT="Trace"/>
+<node CREATED="1344944398881" ID="ID_1604566464" MODIFIED="1344944402600" TEXT="Init"/>
+<node CREATED="1344944403138" ID="ID_925127897" MODIFIED="1344944406569" TEXT="New"/>
+<node CREATED="1344944406971" ID="ID_1060657385" MODIFIED="1344944408655" TEXT="Filter"/>
+</node>
+<node CREATED="1344946365910" ID="ID_1132964144" MODIFIED="1344946369304" TEXT="Artifacts">
+<node CREATED="1344946370911" ID="ID_321211884" MODIFIED="1344946376778" TEXT="RTM"/>
+<node CREATED="1344946377389" ID="ID_786319509" MODIFIED="1344946382754" TEXT="List"/>
+<node CREATED="1344946383231" ID="ID_1170583273" MODIFIED="1344946388178" TEXT="Overview graph"/>
+</node>
+<node CREATED="1344946517705" ID="ID_1511897714" MODIFIED="1344946525471" TEXT="Usage"/>
+<node CREATED="1345063022702" ID="ID_1697401512" MODIFIED="1345063026421" TEXT="Installation">
+<node CREATED="1345063027795" ID="ID_1454450401" MODIFIED="1345063041253" TEXT="Linux/Unix"/>
+<node CREATED="1345063036328" ID="ID_693031096" MODIFIED="1345063038984" TEXT="Windows"/>
+<node CREATED="1345063042440" ID="ID_1869882075" MODIFIED="1345063043881" TEXT="Mac"/>
+</node>
+<node CREATED="1345063092743" ID="ID_879396539" MODIFIED="1345063097055" TEXT="Introduction">
+<node CREATED="1345063097061" ID="ID_674489658" MODIFIED="1345063101815" TEXT="Philosophy"/>
+<node CREATED="1345063102596" ID="ID_981993283" MODIFIED="1345063108392" TEXT="Inspiration"/>
+<node CREATED="1345063151087" ID="ID_916131634" MODIFIED="1345063155076" TEXT="Filesystem"/>
+<node CREATED="1345063155425" ID="ID_38423243" MODIFIED="1345063159723" TEXT="Config files"/>
+</node>
+</node>
+<node CREATED="1344943613050" ID="ID_623882596" MODIFIED="1344943618132" POSITION="right" TEXT="Get involved">
+<node CREATED="1344943798326" ID="ID_10892398" MODIFIED="1344943801566" TEXT="Developer"/>
+<node CREATED="1344943802264" ID="ID_663625161" MODIFIED="1344943816342" TEXT="User"/>
+</node>
+<node CREATED="1344943619675" ID="ID_191319295" MODIFIED="1344943841706" POSITION="left" TEXT="Contact"/>
+<node CREATED="1344943849379" ID="ID_1563348432" MODIFIED="1344943859679" POSITION="right" TEXT="Download (link to github)"/>
+<node CREATED="1344946397567" ID="ID_1426776249" MODIFIED="1344946404315" POSITION="left" TEXT="Quick start"/>
+<node CREATED="1345063365871" ID="ID_1560257093" MODIFIED="1345063378974" POSITION="right" TEXT="Film on frontpage explaining how it works"/>
+<node CREATED="1345063466101" ID="ID_1686414847" MODIFIED="1345063493022" POSITION="right" TEXT="Make reqy repo for site requirements?">
+<node CREATED="1345063493030" ID="ID_474485354" MODIFIED="1345114643605" TEXT="De">
+<font BOLD="true" NAME="Dialog"/>
+</node>
+<node CREATED="1345114645013" ID="ID_499109861" MODIFIED="1345207472174" TEXT="Define what reqy is">
+<node CREATED="1345114655130" ID="ID_1271625980" MODIFIED="1345114661957" TEXT="Don&apos;t compete on features"/>
+<node CREATED="1345114662356" ID="ID_311759846" MODIFIED="1345114667066" TEXT="Compete on experience?"/>
+<node CREATED="1345114695797" ID="ID_1886601153" MODIFIED="1345114705484" TEXT="Reqy is lightweight requirements management"/>
+<node CREATED="1345114706832" ID="ID_526666009" MODIFIED="1345114723643" TEXT="Reqy is a way to connect project documentation to requirements"/>
+<node CREATED="1345114724057" ID="ID_1626714933" MODIFIED="1345114742727" TEXT="Reqy is a way to manage what requirements get made"/>
+<node CREATED="1345114745065" ID="ID_514832223" MODIFIED="1345114750224" TEXT="Reqy is file based"/>
+<node CREATED="1345114753477" ID="ID_1674761410" MODIFIED="1345114758595" TEXT="Reqy does one thing"/>
+<node CREATED="1345114784046" ID="ID_1126103297" MODIFIED="1345114791062" TEXT="Define reqys main usecase">
+<node CREATED="1345114792266" ID="ID_562290771" MODIFIED="1345114814465" TEXT="Managing requirements for a project"/>
+</node>
+<node CREATED="1345114852775" ID="ID_68962957" MODIFIED="1345114957984" TEXT="What is reqys tag line?">
+<node CREATED="1345114959311" ID="ID_1800056788" MODIFIED="1345114960631" TEXT=" Lightweight RM?"/>
+<node CREATED="1345114961144" ID="ID_983741418" MODIFIED="1345114977271" TEXT="Connecting requirements to code?"/>
+<node CREATED="1345114977844" ID="ID_336641895" MODIFIED="1345114989170" TEXT="Lightweight traceability"/>
+<node CREATED="1345114989598" ID="ID_1310153387" MODIFIED="1345114995542" TEXT="Simple traceability?"/>
+<node CREATED="1345119577025" ID="ID_215566549" MODIFIED="1345119713944" TEXT="Connecting artifacts">
+<font BOLD="true" NAME="SansSerif" SIZE="12"/>
+</node>
+<node CREATED="1345119714534" ID="ID_411548548" MODIFIED="1345119720175" TEXT="Connecting documents"/>
+<node CREATED="1345119582601" ID="ID_587679936" MODIFIED="1345119588863" TEXT="Linking documents"/>
+<node CREATED="1345211688847" ID="ID_1421643871" MODIFIED="1345211693798" TEXT="Needs management"/>
+</node>
+<node CREATED="1345209283492" ID="ID_68037149" MODIFIED="1345209308126" TEXT="Help ensure everything produced can be traced to a justification(rationale attribute)"/>
+<node CREATED="1345207478027" ID="ID_332726819" MODIFIED="1345207572496" TEXT="Explain how can work with agile -&gt; need to know what the capabilities are in order to know what to make, issue with agile, but not same as everything having to be done upfront, can start with most important capabilities and then complete them iteratively using agile"/>
+<node CREATED="1345209801661" ID="ID_1854545030" MODIFIED="1345209884566" TEXT="Why need requirements - problem with user stories is that they are often a solution and are not traceable to businessneeds/capabilities. Thats why passengers don&apos;t create aircraft."/>
+<node CREATED="1345211104606" ID="ID_436395081" MODIFIED="1345211154859" TEXT="Reqy is intended not for documenting what the system does, that is best described in code;) Reqy should be used to document the reasoning behind a system."/>
+<node CREATED="1345209956761" ID="ID_1559080426" MODIFIED="1345209973291" TEXT="Requirements don&apos;t describe what will be made, they describe why."/>
+<node CREATED="1345210386399" ID="ID_1054529484" MODIFIED="1345210929409" TEXT="Are capabilities a type of document or are they just a requirements package, should they have their own class (same as package). Because capabilities may affect/be cause of many different packages and requirements throughout the tree perhaps they should be a document type? Userstories/usecases must be traceable back to capabilities, can do this by having a top node capability with child capabilities then a top nod requirement and child requirements, for the child requirement it can trace back to a capability and also to it&apos;s document. If capabilities then perhaps attributes for Business value for investing (e.g. 1-5) and business damage of not investing (e.g 1-5), can then make table with ratio showing which capabilities have best value for money"/>
+<node CREATED="1345211638647" ID="ID_1293405659" MODIFIED="1345211686258" TEXT="Reqy helps agile teams by allowing them to record the customer needs and link them to artifacts"/>
+<node CREATED="1345211478890" ID="ID_854575553" MODIFIED="1345211613712" TEXT="Problem with agile product owner is that they make user stories based on what they want(solutions), they are not the business owner and are often not able to see how to realize the buisness value bacause they are not skilled business analysts that are trained to identify what the solution must do to deliver value."/>
+<node CREATED="1345210982425" ID="ID_235716201" MODIFIED="1345211044754" TEXT="Agile is good for rapibly making things, but it needs direction in order to make the right thing. Agile devs focus on velocity and making functionality, capabilitiy based planning focuses on making the right thing."/>
+<node CREATED="1345207585755" ID="ID_1790453121" MODIFIED="1345207799423" TEXT="Help ensure all artifacts are traceable to requirements"/>
+<node CREATED="1345208882427" ID="ID_954249026" MODIFIED="1345208902113" TEXT="A way to describe what the needs/problems are, not the solutions"/>
+<node CREATED="1345115064298" ID="ID_424634442" MODIFIED="1345115066864" TEXT="don&apos;t let users tell you solutions; only let them tell you problems"/>
+<node CREATED="1345209583983" ID="ID_848570247" MODIFIED="1345209621698" TEXT="Reqy only as good as it&apos;s requirements, users need to add requirements NOT solutions, i.e. the why NOT the how!"/>
+<node CREATED="1345115143331" ID="ID_611883975" MODIFIED="1345115152410" TEXT="Who will use reqy and why?">
+<node CREATED="1345115158148" ID="ID_1308513457" MODIFIED="1345115164102" TEXT="Ideal customer?"/>
+<node CREATED="1345115233238" ID="ID_325873440" MODIFIED="1345115272376" TEXT="A lone PM that assembles and tracks requirements that need to be communicated to others using common/free tools?"/>
+<node CREATED="1345115331528" ID="ID_264063364" MODIFIED="1345115349823" TEXT="Technically minded geek that wants to better document their project?">
+<node CREATED="1345115352155" ID="ID_1641302990" MODIFIED="1345115358625" TEXT="How does reqy help do that?"/>
+</node>
+<node CREATED="1345115367521" ID="ID_1360847741" MODIFIED="1345115404188" TEXT="Someone who is tired of sprawling file repositories containing much redundant documentation?"/>
+<node CREATED="1345116010829" ID="ID_1676898390" MODIFIED="1345116026413" TEXT="Sysadmins who want to document their systems?"/>
+</node>
+<node CREATED="1345119214483" ID="ID_468160792" MODIFIED="1345119259364" TEXT="Does it have to be command line only? Gui / web interface? why define by the tech?"/>
+</node>
+</node>
+</node>
+</map>
